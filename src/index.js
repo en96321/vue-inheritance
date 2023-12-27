f833fd98ae06c1704ca9792c7590e354c5b90784
en96321 <en96321@gmail.com>
2023-12-27 11:29:15 +0800

    feature: add merge data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { mergeRight, clone } from 'ramda'
 
+const isFunction = val => typeof val === 'function'
+
 export class VueInheritance {
   constructor ({
+    data,
     props,
     methods,
     computed
   }) {
+    this.data = clone(data)
     this.props = clone(props)
     this.methods = clone(methods)
     this.computed = clone(computed)
@@ -21,6 +25,13 @@ export class VueInheritance {
   extend (extendOptions) {
     const cloneExtendOptions = clone(extendOptions)
     try {
+      const currentData = this.data
+      this.data = function () {
+        return mergeRight(
+          isFunction(currentData) ? currentData.call(this) : currentData,
+          isFunction(cloneExtendOptions.data) ? cloneExtendOptions.data.call(this) : cloneExtendOptions.data
+        )
+      }
       this.props = mergeRight(this.props, cloneExtendOptions.props)
       this.methods = mergeRight(this.methods, cloneExtendOptions.methods)
       this.computed = mergeRight(this.computed, cloneExtendOptions.computed)
